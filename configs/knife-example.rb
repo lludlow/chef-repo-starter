276e9a160873e400fe6c7e005b14656ead43d582
Adam D <compybara@users.noreply.github.com>
2018-04-18 20:58:36 -0700

    Clean up the knife example config
    Forgot to change some details from the knife.rb file I made this template from.

--- configs/knife-example.rb
+++ configs/knife-example.rb
@@ -5,7 +5,7 @@ log_level                :info
 log_location             STDOUT
 node_name                "your_username"
 client_key               "#{current_dir}/your_key.pem"
-validation_client_name   "example_org-validator.pem"
-validation_key           "#{current_dir}/sl_techops_tahoeprod_01-validator.pem" # Can be found in 1password
+validation_client_name   "example_org-validator"
+validation_key           "#{current_dir}/example_org-validator.pem" # Can be found in 1password
 chef_server_url          "https://chef.example.com/organizations/example_org"
 cookbook_path            ["#{current_dir}/../cookbooks"]
